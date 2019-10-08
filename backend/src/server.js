41bab530a3350af482437107bd52a6b734c61e93
alexavier20 <alex_xavier@outlook.com>
2019-10-07 21:14:45 -0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-e0vgb.mongodb.net/semana09?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para ediçao e delete)
+// req.body = Acessar corpo da requisição (para criação e edição)
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
