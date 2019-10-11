58a141f3e6e3ff1aad643cca19a1de70c57c487b
alexavier20 <alex_xavier@outlook.com>
2019-10-10 22:03:29 -0300

    criado formulario New

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 mongoose.connect('mongodb+srv://oministack:oministack@cluster0-e0vgb.mongodb.net/semana09?retryWrites=true&w=majority',{
@@ -15,5 +16,6 @@ mongoose.connect('mongodb+srv://oministack:oministack@cluster0-e0vgb.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 app.listen(3333);
--- frontend/src/router.js
+++
@@ -1,5 +0,0 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import New from './pages/New';
