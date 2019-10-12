42bd8c8a7fa635b17911eb94e31b19c4185d0d3f
alexavier20 <alex_xavier@outlook.com>
2019-10-12 14:09:53 -0300

    termino React

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useMemo } from 'react';
 
 import './styles.css';
+import api from '../../services/api';
 import camera from '../../assets/camera.svg'
 
-export default function New() {
+export default function New({ history }) {
     const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
@@ -13,8 +14,21 @@ export default function New() {
         return thumbnail ? URL.createObjectURL(thumbnail) : null;
     }, [thumbnail])
 
-    function handleSubmit(){
+    async function handleSubmit(event){
+        event.preventDefault();
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
 
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('/spots', data, {
+            headers: { user_id }
+        });
+
+        history.push('/dashboard');
     }
 
     return (
